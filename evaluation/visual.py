5478ee6002aec66ec1a215692089ee170cb40dca
Manuel Kolmet <manuel.kolmet@gmail.com>
2020-11-01 17:39:44 +0100

    final commit

--- evaluation/visual.py
+++ evaluation/visual.py
@@ -19,7 +19,7 @@ from dataloading.data_loading import Semantic3dDataset
 from retrieval import networks
 from retrieval.netvlad import NetVLAD, EmbedNet
 
-#from evaluation.utils import evaluate_topK, generate_sanity_check_dataset
+from evaluation.utils import evaluate_topK, generate_sanity_check_dataset
 from evaluation.utils import reduce_topK, print_topK
 import evaluation.utils
 
@@ -113,6 +113,66 @@ def eval_netvlad_retrieval(dataset_train, dataset_test, netvlad_vectors_train, n
 
     return thresh_hits, scene_hits
 
+def eval_netvlad_retrieval_old(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test, top_k=(1,3,5,10), reduce_indices=None):
+    assert reduce_indices in (None,'scene-voting','scene-voting-double-k')
+    print(f'eval_netvlad_retrieval(): # training: {len(dataset_train)}, # test: {len(dataset_test)}')
+    print('Reduce indices:',reduce_indices)
+
+    retrieval_dict={}
+
+    image_positions_train, image_orientations_train = dataset_train.image_positions, dataset_train.image_orientations
+    image_positions_test, image_orientations_test = dataset_test.image_positions, dataset_test.image_orientations
+    scene_names_train = dataset_train.image_scene_names
+    scene_names_test  = dataset_test.image_scene_names
+
+    #Sanity check
+    #netvlad_vectors_train, netvlad_vectors_test, image_positions_train, image_positions_test, image_orientations_train, image_orientations_test, scene_names_train, scene_names_test=generate_sanity_check_dataset()
+
+    pos_results  ={k:[] for k in top_k}
+    ori_results  ={k:[] for k in top_k}
+    scene_results={k:[] for k in top_k}
+        
+    test_indices=np.arange(len(dataset_test))    
+    for test_index in test_indices:
+        scene_name_gt=scene_names_test[test_index]
+
+        netvlad_diffs=netvlad_vectors_train-netvlad_vectors_test[test_index]
+        netvlad_diffs=np.linalg.norm(netvlad_diffs,axis=1)   
+
+        pos_dists=np.linalg.norm(image_positions_train[:]-image_positions_test[test_index], axis=1) #CARE: also adds z-distance
+        ori_dists=np.abs(image_orientations_train[:]-image_orientations_test[test_index])
+        ori_dists=np.minimum(ori_dists, 2*np.pi-ori_dists)
+
+        for k in top_k:
+            if reduce_indices is None:
+                sorted_indices=np.argsort(netvlad_diffs)[0:k] #Sanity still same result ✓
+            if reduce_indices =='scene-voting':
+                sorted_indices=np.argsort(netvlad_diffs)[0:k]
+                sorted_indices=evaluation.utils.reduceIndices_sceneVoting(scene_names_train, sorted_indices)
+            if reduce_indices =='scene-voting-double-k':
+                sorted_indices=np.argsort(netvlad_diffs)[0:k]
+                sorted_indices_voting=np.argsort(netvlad_diffs)[ k : 2*k ] # Take another k top retrievals just for scene-voting to compare to combined models
+                sorted_indices_topK,sorted_indices_doubleK=evaluation.utils.reduceIndices_sceneVoting(scene_names_train, sorted_indices, sorted_indices_voting)
+                sorted_indices = sorted_indices_topK if len(sorted_indices_topK)>0 else sorted_indices_doubleK #Same logic as in visual_geometric, trust next indices if they are "united" enough to over-rule top-k
+
+            if k==np.max(top_k): retrieval_dict[test_index]=sorted_indices                
+
+            scene_correct=np.array([scene_name_gt == scene_names_train[retrieved_index] for retrieved_index in sorted_indices])
+            topk_pos_dists=pos_dists[sorted_indices]
+            topk_ori_dists=ori_dists[sorted_indices]    
+
+            #Append the average pos&ori. errors *for the cases that the scene was hit*
+            pos_results[k].append( np.mean( topk_pos_dists[scene_correct==True]) if np.sum(scene_correct)>0 else None )
+            ori_results[k].append( np.mean( topk_ori_dists[scene_correct==True]) if np.sum(scene_correct)>0 else None )
+            scene_results[k].append( np.mean(scene_correct) ) #Always append the scene-scores
+    
+    assert len(pos_results[k])==len(ori_results[k])==len(scene_results[k])==len(test_indices)
+
+    print('Saving retrieval results...')
+    pickle.dump(retrieval_dict, open('retrievals_NV-S3D.pkl','wb'))
+
+    return evaluate_topK(pos_results, ori_results, scene_results)    
+
 
 #TODO sanity checks: same scene-acc, 100% for large thresh, 0% for small thresh
 if __name__ == "__main__":
@@ -190,16 +250,25 @@ if __name__ == "__main__":
 
         features_name='features_netvlad_Occ-Occ_m0.5_o0.3.pkl'
         netvlad_vectors_train,netvlad_vectors_test=pickle.load(open('evaluation_res/'+features_name,'rb')); print(f'Using features {features_name}')
-        thresh_results, scene_results=eval_netvlad_retrieval(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test, reduce_indices=None)
-        print_topK(thresh_results, scene_results)     
-        thresh_results, scene_results=eval_netvlad_retrieval(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test, reduce_indices='scene-voting')
+        thresh_results, scene_results=eval_netvlad_retrieval(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test,top_k=(1,3,5,10), reduce_indices=None)
         print_topK(thresh_results, scene_results)  
+        thresh_results, scene_results=eval_netvlad_retrieval(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test,top_k=(10,15,20), reduce_indices=None)
+        print_topK(thresh_results, scene_results) 
+
+        thresh_results, scene_results=eval_netvlad_retrieval(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test,top_k=(1,3,5,10), reduce_indices='scene-voting')
+        print_topK(thresh_results, scene_results) 
+        thresh_results, scene_results=eval_netvlad_retrieval(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test,top_k=(10,15,20), reduce_indices='scene-voting')
+        print_topK(thresh_results, scene_results)         
 
     if 'netvlad-pitts' in sys.argv:
         features_name='features_netvlad_Pitts.pkl'
         netvlad_vectors_train,netvlad_vectors_test=pickle.load(open('evaluation_res/'+features_name,'rb')); print(f'Using features {features_name}')
         thresh_results, scene_results=eval_netvlad_retrieval(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test, reduce_indices=None)
-        print_topK(thresh_results, scene_results)                                       
+        print_topK(thresh_results, scene_results)    
+
+        print('\n OLD:')
+        pos_results, ori_results, scene_results=eval_netvlad_retrieval_old(dataset_train, dataset_test, netvlad_vectors_train, netvlad_vectors_test, top_k=(1,3,5,10), reduce_indices=None)
+        print(pos_results, ori_results, scene_results)                                           
                              
     if 'gather-cambridge' in sys.argv:
         IMAGE_LIMIT=3000
